41c2f57e216858755708fdd7f6ac3b0eecd7d891
Aman Gupta <amanmacair98@gmail.com>
2026-03-23 00:38:52 +0530

    Update auth routes

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -6,6 +6,7 @@ from models.user_model import User
 from schemas.user_schema import UserCreate, UserResponse, UserLogin
 from utils.password_hash import hash_password, verify_password
 from utils.jwt_handler import create_access_token
+from utils.helpers import get_current_user
 from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
@@ -64,3 +65,11 @@ def login_user(
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+
+# -------------------------
+# GET CURRENT USER (ME)
+# -------------------------
+@router.get("/me", response_model=UserResponse)
+def get_current_user_info(current_user: User = Depends(get_current_user)):
+    return current_user
